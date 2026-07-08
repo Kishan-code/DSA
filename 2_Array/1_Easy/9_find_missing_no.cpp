acbaeb0c76ab8ab45f682668d283f2d885058158
Kishan-code <kumarkishan78254@gmail.com>
2026-07-08 23:21:32 +0530

    Array -> Easy -> Problem 9 solve using Better approach (hash map)

--- 2_Array/1_Easy/9_find_missing_no.cpp
+++ 2_Array/1_Easy/9_find_missing_no.cpp
@@ -20,15 +20,28 @@
  * 
  * TC -> if array is sorted then -> O(n) otherwise O(logn)+O(n)
  * SC -> O(1)
+ * ************************************************************************
+ * 
+ * Approach -2: Better (using hashmap):
+ * create a map and store all the element as: map[arr[i]] = arr[i];
+ * here key and value is same.
+ * now iterate the map and form j = 0 to j = n
+ * if map[j] is present then j++
+ * otherwise break the loop
+ * and return j;
+ * 
+ * TC -> O(2n)
+ * SC -> O(n)
  */
 
 
  #include<iostream>
  #include<vector>
+ #include<map>
  #include<algorithm>
  using namespace std;
 
- int findMissingNum(vector<int> &v){
+ int findMissingNum1(vector<int> &v){
     sort(v.begin(), v.end());
 
     int i = 0;
@@ -42,9 +55,28 @@
     return i;
  }
 
+ int findMissingNum2(vector<int> &v){
+    map<int, int> mp;
+
+    for(int i = 0; i < v.size(); i++){
+        mp[v[i]] = v[i];  
+    }
+
+    int j = 0;
+    while(j < v.size()){
+        if(!mp.count(j)){
+            break;
+        }
+        j++;
+    }
+    
+    return j;
+ }
+
  int main(){
 
     vector<int> arr = {0, 1, 2, 4, 5, 6};
-    cout<<findMissingNum(arr);
+    // cout<<findMissingNum1(arr);
+    cout<<findMissingNum2(arr);
     return 0;
  }
