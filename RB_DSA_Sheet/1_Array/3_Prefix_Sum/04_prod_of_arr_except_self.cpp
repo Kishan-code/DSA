8ac502a94edbd4d4a96d4e20cefd97d8d7674b72
Kishan-code <kumarkishan78254@gmail.com>
2026-09-18 15:15:15 +0530

    RB_DSA_Sheet -> Array -> Prefix Sum -> Problem 4 Product of array except self solve

---
+++ RB_DSA_Sheet/1_Array/3_Prefix_Sum/04_prod_of_arr_except_self.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+/*
+Leetcode: Problem-238: Product of Array Except Self
+****************************************************************************************
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+2 <= nums.length <= 10⁵
+-30 <= nums[i] <= 30
+The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+
+*/
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans(n);
+    int prev = 1;
+    int mul;
+
+    for(int i = 0; i < n; i++){
+        if(i == 0)
+            ans[i] = prev;
+        else{
+            ans[i] = prev * ans[i-1];
+        }
+        prev = nums[i];
+    }
+
+    for(int i = n-2; i >= 0; i--){
+        ans[i] *= prev;
+        prev *= nums[i];
+    }
+
+    return ans;
+
+}
+
+/******************************************************************************* */
+
+int main(void){
+    vector<int> nums = {1,2,3,4};
+
+    vector<int> ans = productExceptSelf(nums);
+
+    for(auto x: ans)
+        cout<<x<<" ";
+        
+    return 0;
+}
